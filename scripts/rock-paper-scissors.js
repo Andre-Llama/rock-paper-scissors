9ca0cfe58133f4a9e15c48010d3837c98316a5e2
Andre Llama <drellama72@gmail.com>
2021-02-22 08:54:19 -0500

    Added rock-paper-scissors.html and script rock-paper-scissors.js

---
+++ scripts/rock-paper-scissors.js
@@ -0,0 +1,14 @@
+// randomly returns either 'Rock', 'Paper', or 'Scissors'
+function computerPlay() {
+
+}
+
+// plays a single round of Rock Paper Scissors and declares the winner of the round
+function playRound(playerSelection, computerSelection) {
+
+}
+
+// plays a 5 round game and declares the winner at the end
+function game() {
+  
+}
